a86cdcc704e42071171cbd18890a1c0e9b126d33
Admin <tiago.rafael019@gmail.com>
2025-06-25 15:27:44 -0300

    Adionando pasta de user

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 
 // Controller
 import { AppController } from './app.controller';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [AppController],
   providers: [],
 })
