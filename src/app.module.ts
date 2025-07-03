794de74fadbce253b208bd21364f820fe6b723ed
Admin <tiago.rafael019@gmail.com>
2025-07-03 16:11:49 -0300

    done

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 
 // Controler
 import { AppController } from './app.controller';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule],
   controllers: [AppController],
   providers: [],
 })
