e5ce81d06ec06f582d93b02305cd1ea97d9d6d74
Admin <tiago.rafael019@gmail.com>
2025-07-03 16:00:18 -0300

    done

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -4,7 +4,6 @@ import { Global, Module } from '@nestjs/common';
 // Service
 import { PrismaService } from './prisma.service';
 
-@Global()
 @Module({
   providers: [PrismaService],
   exports: [PrismaService],
