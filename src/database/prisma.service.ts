fa752e90b95217ee290782e6340114e6b57fd095
Admin <tiago.rafael019@gmail.com>
2025-07-04 09:14:51 -0300

    Bug fix in Prisma in production environment

--- src/database/prisma.service.ts
+++ src/database/prisma.service.ts
@@ -2,7 +2,7 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 
 // Bibliotecas
-import { PrismaClient } from 'generated/prisma';
+import { PrismaClient } from '../../generated/prisma';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
